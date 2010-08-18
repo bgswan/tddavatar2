73270f19d6d24c9b543919aaead99cdf6d9ec619
Brian Swan <brianswan@Brian-Swans-MacBook.local>
2010-08-18 20:04:12 +0100

    Added initial appointment test

---
+++ appointment_spec.rb
@@ -0,0 +1,13 @@
+require 'spec/test/unit'
+
+describe Appointment do
+  
+  it "is created with owner and patient"
+  
+  it "charges for treatments"
+  
+  it "settles charges with cash"
+  
+  it "prints a receipt"
+  
+end
