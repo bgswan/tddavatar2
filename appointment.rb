fcfc2a9982343cc4857dc210ef7b2d1e78f9cbac
Brian Swan <bgswan@gmail.com>
2010-08-18 22:11:50 +0100

    Implemented appointment methods to pass failing test

--- appointment.rb
+++ appointment.rb
@@ -5,11 +5,14 @@ class Appointment
   def initialize(details={})
     @patient = details[:patient]
     @owner = details[:owner]
+    @treatments = []
   end
   
   def charge_for(treatment)
+    @treatments << treatment
   end
   
   def total_due
+    @treatments.inject(0) { |total, treatment| total += treatment.cost }
   end
 end
